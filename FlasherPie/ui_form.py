c68366c6b83cb81665fbfe3ffd7d50e9fb890755
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-08-27 16:07:45 +0500

    optimize ui_form.py

--- FlasherPie/ui_form.py
+++ FlasherPie/ui_form.py
@@ -1,166 +1,118 @@
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'form.ui'
-##
-## Created by: Qt User Interface Compiler version 6.7.2
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
-
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFormLayout, QLabel, QListWidget,
-    QListWidgetItem, QPushButton, QSizePolicy, QVBoxLayout,
-    QWidget)
-
+from PySide6.QtCore import QCoreApplication, QRect, QMetaObject
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QListWidget, QLabel, QFormLayout
 from .button import CustomButton
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
-        if not Widget.objectName():
-            Widget.setObjectName(u"Widget")
+        Widget.setObjectName("Widget")
         Widget.resize(480, 800)
-        Widget.setStyleSheet(u"background-color: rgb(127, 127, 127);")
-        self.widget = QWidget(Widget)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(40, 10, 401, 781))
-        self.verticalLayout_3 = QVBoxLayout(self.widget)
-        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.flashListWidget = QListWidget(self.widget)
-        self.flashListWidget.setObjectName(u"flashListWidget")
-        self.flashListWidget.setStyleSheet(u"background-color: rgb(200, 200, 200); color: black;")
+        Widget.setStyleSheet("background-color: rgb(127, 127, 127);")
 
-        self.verticalLayout_3.addWidget(self.flashListWidget)
-
-        self.formLayout = QFormLayout()
-        self.formLayout.setObjectName(u"formLayout")
-        self.label = QLabel(self.widget)
-        self.label.setObjectName(u"label")
-        self.label.setStyleSheet(u"color: black;")
-
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label)
-
-        self.titleLabel = QLabel(self.widget)
-        self.titleLabel.setObjectName(u"titleLabel")
-        self.titleLabel.setStyleSheet(u"color: black;")
-
-        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.titleLabel)
-
-        self.label_2 = QLabel(self.widget)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setStyleSheet(u"color: black;")
-
-        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label_2)
-
-        self.versionLabel = QLabel(self.widget)
-        self.versionLabel.setObjectName(u"versionLabel")
-        self.versionLabel.setStyleSheet(u"color: black;")
-
-        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.versionLabel)
-
-        self.label_3 = QLabel(self.widget)
-        self.label_3.setObjectName(u"label_3")
-        self.label_3.setStyleSheet(u"color: black;")
-
-        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label_3)
-
-        self.dateLabel = QLabel(self.widget)
-        self.dateLabel.setObjectName(u"dateLabel")
-        self.dateLabel.setStyleSheet(u"color: black;")
-
-        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.dateLabel)
-
-        self.label_4 = QLabel(self.widget)
-        self.label_4.setObjectName(u"label_4")
-        self.label_4.setStyleSheet(u"color: black;")
-
-        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.label_4)
-
-        self.descriptionLabel = QLabel(self.widget)
-        self.descriptionLabel.setObjectName(u"descriptionLabel")
-        self.descriptionLabel.setStyleSheet(u"color: black;")
-
-        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.descriptionLabel)
-
-
-        self.verticalLayout_3.addLayout(self.formLayout)
-
-        self.logListWidget = QListWidget(self.widget)
-        self.logListWidget.setObjectName(u"logListWidget")
-        self.logListWidget.setStyleSheet(u"background-color: rgb(50, 50, 50); color: white;")
-
-        self.verticalLayout_3.addWidget(self.logListWidget)
-
-        self.widget1 = QWidget(Widget)
-        self.widget1.setObjectName(u"widget1")
-        self.widget1.setGeometry(QRect(440, 0, 41, 801))
-        self.verticalLayout_2 = QVBoxLayout(self.widget1)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.powerButton = CustomButton(self.widget1)
-        self.powerButton.setObjectName(u"powerButton")
-
-        self.verticalLayout_2.addWidget(self.powerButton)
-
-        self.upButton = CustomButton(self.widget1)
-        self.upButton.setObjectName(u"upButton")
-
-        self.verticalLayout_2.addWidget(self.upButton)
-
-        self.downButton = CustomButton(self.widget1)
-        self.downButton.setObjectName(u"downButton")
-
-        self.verticalLayout_2.addWidget(self.downButton)
-
-        self.enterButton = CustomButton(self.widget1)
-        self.enterButton.setObjectName(u"enterButton")
-
-        self.verticalLayout_2.addWidget(self.enterButton)
-
-        self.widget2 = QWidget(Widget)
-        self.widget2.setObjectName(u"widget2")
-        self.widget2.setGeometry(QRect(0, 0, 41, 801))
-        self.verticalLayout = QVBoxLayout(self.widget2)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.menuButton = CustomButton(self.widget2)
-        self.menuButton.setObjectName(u"menuButton")
-
-        self.verticalLayout.addWidget(self.menuButton)
-
-        self.flashButton = CustomButton(self.widget2)
-        self.flashButton.setObjectName(u"flashButton")
-
-        self.verticalLayout.addWidget(self.flashButton)
-
-        self.eraseButton = CustomButton(self.widget2)
-        self.eraseButton.setObjectName(u"eraseButton")
-
-        self.verticalLayout.addWidget(self.eraseButton)
-
-        self.exitButton = CustomButton(self.widget2)
-        self.exitButton.setObjectName(u"exitButton")
-
-        self.verticalLayout.addWidget(self.exitButton)
-
-
+        self.setupMainWidget(Widget)
+        self.setupRightSideButtons(Widget)
+        self.setupLeftSideButtons(Widget)
         self.retranslateUi(Widget)
 
         QMetaObject.connectSlotsByName(Widget)
-    # setupUi
+
+    def setupMainWidget(self, parent):
+        self.widget = QWidget(parent)
+        self.widget.setGeometry(QRect(40, 10, 401, 781))
+        self.layout = QVBoxLayout(self.widget)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+        self.flashListWidget = QListWidget(self.widget)
+        self.flashListWidget.setStyleSheet("background-color: rgb(200, 200, 200); color: black;")
+        self.layout.addWidget(self.flashListWidget)
+
+        self.formLayout = QFormLayout()
+        self.setupForm(self.widget)
+        self.layout.addLayout(self.formLayout)
+
+        self.logListWidget = QListWidget(self.widget)
+        self.logListWidget.setStyleSheet("background-color: rgb(50, 50, 50); color: white;")
+        self.layout.addWidget(self.logListWidget)
+
+    def setupForm(self, parent):
+        self.label = QLabel(parent)
+        self.label.setStyleSheet("color: black;")
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label)
+
+        self.titleLabel = QLabel(parent)
+        self.titleLabel.setStyleSheet("color: black;")
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.titleLabel)
+
+        self.label_2 = QLabel(parent)
+        self.label_2.setStyleSheet("color: black;")
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label_2)
+
+        self.versionLabel = QLabel(parent)
+        self.versionLabel.setStyleSheet("color: black;")
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.versionLabel)
+
+        self.label_3 = QLabel(parent)
+        self.label_3.setStyleSheet("color: black;")
+        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label_3)
+
+        self.dateLabel = QLabel(parent)
+        self.dateLabel.setStyleSheet("color: black;")
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.dateLabel)
+
+        self.label_4 = QLabel(parent)
+        self.label_4.setStyleSheet("color: black;")
+        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.label_4)
+
+        self.descriptionLabel = QLabel(parent)
+        self.descriptionLabel.setStyleSheet("color: black;")
+        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.descriptionLabel)
+
+    def setupRightSideButtons(self, parent):
+        self.widgetRight = QWidget(parent)
+        self.widgetRight.setGeometry(QRect(440, 0, 41, 801))
+        self.layoutRight = QVBoxLayout(self.widgetRight)
+        self.layoutRight.setContentsMargins(0, 0, 0, 0)
+
+        self.powerButton = CustomButton(self.widgetRight)
+        self.powerButton.setObjectName("powerButton")
+        self.layoutRight.addWidget(self.powerButton)
+
+        self.upButton = CustomButton(self.widgetRight)
+        self.upButton.setObjectName("upButton")
+        self.layoutRight.addWidget(self.upButton)
+
+        self.downButton = CustomButton(self.widgetRight)
+        self.downButton.setObjectName("downButton")
+        self.layoutRight.addWidget(self.downButton)
+
+        self.enterButton = CustomButton(self.widgetRight)
+        self.enterButton.setObjectName("enterButton")
+        self.layoutRight.addWidget(self.enterButton)
+
+    def setupLeftSideButtons(self, parent):
+        self.widgetLeft = QWidget(parent)
+        self.widgetLeft.setGeometry(QRect(0, 0, 41, 801))
+        self.layoutLeft = QVBoxLayout(self.widgetLeft)
+        self.layoutLeft.setContentsMargins(0, 0, 0, 0)
+
+        self.menuButton = CustomButton(self.widgetLeft)
+        self.menuButton.setObjectName("menuButton")
+        self.layoutLeft.addWidget(self.menuButton)
+
+        self.flashButton = CustomButton(self.widgetLeft)
+        self.flashButton.setObjectName("flashButton")
+        self.layoutLeft.addWidget(self.flashButton)
+
+        self.eraseButton = CustomButton(self.widgetLeft)
+        self.eraseButton.setObjectName("eraseButton")
+        self.layoutLeft.addWidget(self.eraseButton)
+
+        self.exitButton = CustomButton(self.widgetLeft)
+        self.exitButton.setObjectName("exitButton")
+        self.layoutLeft.addWidget(self.exitButton)
 
     def retranslateUi(self, Widget):
-        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
-        self.label.setText(QCoreApplication.translate("Widget", u"Qurilma:", None))
-        self.label_2.setText(QCoreApplication.translate("Widget", u"Versiya:", None))
-        self.label_3.setText(QCoreApplication.translate("Widget", u"Sana:", None))
-        self.label_4.setText(QCoreApplication.translate("Widget", u"Tavsif:", None))
-    # retranslateUi
-
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", "Widget"))
+        self.label.setText(QCoreApplication.translate("Widget", "Qurilma:"))
+        self.label_2.setText(QCoreApplication.translate("Widget", "Versiya:"))
+        self.label_3.setText(QCoreApplication.translate("Widget", "Sana:"))
+        self.label_4.setText(QCoreApplication.translate("Widget", "Tavsif:"))
