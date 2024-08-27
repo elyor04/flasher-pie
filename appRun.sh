8e0cfacd040f4cbc7767515e7a1c761da21b93d4
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-08-28 00:48:42 +0500

    fix

--- appRun.sh
+++ appRun.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
 BASE_DIR=$(dirname "$(readlink -f "$0")")
+
+echo "RUNNING: cd "$BASE_DIR""
 cd "$BASE_DIR"
 
+echo "RUNNING: git pull"
 git pull
+
+echo "RUNNING: python3 -m venv .venv"
 python3 -m venv .venv
 
-sleep 1
+echo "RUNNING: source .venv/bin/activate"
 source .venv/bin/activate
 
+echo "RUNNING: pip3 install -r requirements.txt"
 pip3 install -r requirements.txt
+
+echo "RUNNING: python3 main.py"
 python3 main.py
